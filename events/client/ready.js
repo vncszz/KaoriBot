d00bd0a1ed0840326f6f3582d5540bed544bd760
mav <ademarvincius01@gmail.com>
2023-03-02 12:20:30 -0300

    update

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,22 +1,19 @@
-const express = require('express');
+const {Client} = require('discord.js');
 const mongoose = require('mongoose');
-const app = express();
+const config = require('../../config.json');
 
-const uri = 'mongodb+srv://NeoGenesis:SU4NLCVvuT7UkhwA@animesbrasil.frj4gm7.mongodb.net/?retryWrites=true&w=majority'
+module.exports = {
+    name: "ready",
+    once: true,
+    async execute(client) {
+        await mongoose.connect(config.mongodb || '' {
+            keepAlive: true,
+        });
 
-async function connect() {
-    try {
-        await mongoose.connect(uri)
-        console.log('Conectado ao MongoDB');
-    } catch (error) {
-        console.error('erro');
+        if(mongoose.connect) {
+            console.log('Mongodb connected');
+        }
+
+        console.log(`${client.user.username} online.`)
     }
 }
-
-connect();
-
-
-app.listen(8000,() => { 
-    console.log('server iniciado na porta 8000');
-});
-
