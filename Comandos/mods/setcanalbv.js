a8dd9d3bf2d1bc99399d9c44eeddd34255c1dc64
mav <ademarvincius01@gmail.com>
2023-03-05 10:39:26 -0300

    updates

---
+++ Comandos/mods/setcanalbv.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js')
+const { QuickDB } = require('quick.db');
+const db = new QuickDB();
+
+module.exports = {
+
+    name: 'set-canal-bv',
+    description: "[👑] Setar o canal de Boas Vindas.",
+    options: [
+        {
+            name: 'canal',
+            description: 'Mencione o canal',
+            type: Discord.ApplicationCommandOptionType.Channel,
+            channelTypes: [
+                Discord.ChannelType.GuildText
+            ],
+            required: true,
+        }
+    ],
+
+    run: async (client, interaction) => {
+
+        if (!interaction.member.permissions.has(Discord.PermissionFlagsBits.Administrator)) {
+            interaction.reply({ content: `**❌ - Você não possui permissão para utilizar este comando.**`, ephemeral: true })
+        } else {
+
+            let channel = interaction.options.getChannel('canal')
+
+            if (!channel.send)
+            return interaction.reply({
+                content: `**❌ - ${interaction.user}, Você provavelmente selecionou um canal de voz ou categoria. Por favor selecione um canal de texto.**`,
+                ephemeral: true,
+            })
+
+            await db.set('channelwelcome', {channel})
+            
+          let embedchannelbv = new Discord.EmbedBuilder()
+           .setDescription(`**✅ - Canal ${channel} setado para boas vindas**`)
+           .setColor('Random')
+           .setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.user.displayAvatarURL()}`})
+     
+           interaction.reply({ embeds: [embedchannelbv] })
+
+
+        }
+
+    }
+}
--- index.js
+++ index.js
@@ -293,10 +293,10 @@ client.on("messageCreate", (message) => {
       //(message.content.includes(element)) // caso queira que responda com menção em alguma mensagem
 
       let embed = new Discord.EmbedBuilder()
-      .setColor("#000000")
-      .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynaimc: true }) })
-      .setDescription(`🤖 Olá ${message.author}, utilize \`/ajuda\` para ver meus comandos!`)
-      
+        .setColor("#000000")
+        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynaimc: true }) })
+        .setDescription(`🤖 Olá ${message.author}, utilize \`/ajuda\` para ver meus comandos!`)
+
       message.reply({ embeds: [embed] })
     }
   })
@@ -312,31 +312,31 @@ client.on('guildMemberAdd', async member => {
 
   let guild = client.guilds.cache.get('988251099117006878') //ID DO SERVER
   if (guild != member.guild) {
-      
-      console.log({
-          content: `${member.id} entrou em outro servidor!`
-      })
+
+    console.log({
+      content: `${member.id} entrou em outro servidor!`
+    })
   } else {
 
-      
-      let channelid = client.channels.cache.get('1076315570615631994') //ID DO CANAL A ENVIAR BOAS VINDAS
-      let roleReception = '1041164003361169479'
-      
-      const embedmember = new Discord.EmbedBuilder()
-          .setColor('#73CBEC')
-          .setImage('https://cdn.discordapp.com/attachments/1059941914310344845/1062820093354061954/IMG-20230111-WA0016.jpg')
-          .setThumbnail(member.user.displayAvatarURL({dynamic: true}))
-          .setDescription(`**Te desejamos boas vindas ao nosso servidor!**\nLeia nossas [**regras**](https://discord.com/channels/988251099117006878/1076318967368532010) para evitar punições.\nleia nossas [**informações**](https://discord.com/channels/988251099117006878/1076319178211999795) também, lá você ficará informado de tudo possível <:d_02yey:1065719606615998464>`)
-          .setFooter({text: `ID: ${member.id}`, iconURL: member.user.displayAvatarURL({dynamic: true})})
-          .setTimestamp(new Date);
 
-      member.send(`https://discord.gg/QCmhuke4cz\nhttps://discord.gg/Tq6Djum3sP\nhttps://discord.gg/ZaBDu8fXj8\nhttps://discord.gg/utk\nhttps://discord.gg/wildriftbrasil\nhttps://discord.gg/2fWbpPjG2E\nhttps://i.imgur.com/UhQP3Nx.png`)
-      
-      channelid.send({
-          content: `${member} <@&${roleReception}>`,
-          embeds: [embedmember]
-      });
-      
+    //let channelid = client.channels.cache.get('1076315570615631994') //ID DO CANAL A ENVIAR BOAS VINDAS
+
+    let channelMsgBv = await db.get('channelwelcome.channel')
+    let roleReception = '1041164003361169479'
+
+    const embedmember = new Discord.EmbedBuilder()
+      .setColor('#73CBEC')
+      .setImage('https://cdn.discordapp.com/attachments/1059941914310344845/1062820093354061954/IMG-20230111-WA0016.jpg')
+      .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+      .setDescription(`**Te desejamos boas vindas ao nosso servidor!**\nLeia nossas [**regras**](https://discord.com/channels/988251099117006878/1076318967368532010) para evitar punições.\nleia nossas [**informações**](https://discord.com/channels/988251099117006878/1076319178211999795) também, lá você ficará informado de tudo possível <:d_02yey:1065719606615998464>`)
+      .setFooter({ text: `ID: ${member.id}`, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
+      .setTimestamp(new Date);
+
+
+    member.send(`https://discord.gg/QCmhuke4cz\nhttps://discord.gg/Tq6Djum3sP\nhttps://discord.gg/ZaBDu8fXj8\nhttps://discord.gg/utk\nhttps://discord.gg/wildriftbrasil\nhttps://discord.gg/2fWbpPjG2E\nhttps://i.imgur.com/UhQP3Nx.png`)
+
+    await member.guild.channels.cache.get(`${channelMsgBv.id}`).send({ content: `${member} <@&${roleReception}>`, embeds: [embedmember] });
+
   }
-  
+
 })
