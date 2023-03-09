5f643cc6a227e2d7b8d416234a078f2262c10743
mav <ademarvincius01@gmail.com>
2023-03-08 23:57:35 -0300

    Naomi Bot

--- Comandos/utilidades/infobot.js
+++ Comandos/utilidades/infobot.js
@@ -8,17 +8,19 @@ module.exports = {
     run: async (client, interaction) => {
 
         let membros = client.users.cache.size;
+        let servidores = client.guilds.cache.size;
         let canais = client.channels.cache.size;
         let bot = client.user.tag;
         let avatar_bot = client.user.displayAvatarURL({ dynamic: true });
-        
-        const embed = new Discord.EmbedBuilder()
-            .setColor('Purple')
-            .setThumbnail(avatar_bot)
+        let embed = new Discord.EmbedBuilder()
+            .setColor("#000000")
+            .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+            .setAuthor({ name: bot, iconURL: avatar_bot })
             .setFooter({ text: `Pedido por: ${interaction.user.username}` })
-            .setDescription({content: `Olá Sou a ${bot},\nBot Oficial do servidor Animes Zero.\nAtualmente estou supervisionando ${membros} membros.\nTenho acesso a ${canais} canais.`})
+            .setDescription(`Olá ${interaction.user}, veja minhas informações abaixo:\n\n>  Nome: \`${bot}\`\n> ** Owner:** <@523665234351751168>
+\n>  Membros: \`${membros}\`\n>  Servidores: \`${servidores}\`\n>  Canais: \`${canais}\``);
 
-        interaction.reply({ embeds: [embed]})
+        interaction.reply({ embeds: [embed] })
 
 
     }
