b693ec903f35d2b951697492e1f5c19d7619e6de
mav <ademarvincius01@gmail.com>
2023-03-15 18:59:31 -0300

    Naomit

--- Eventos/Client/ready.js
+++ Eventos/Client/ready.js
@@ -5,7 +5,7 @@ const chalk = require("chalk");
 client.on(`ready`, async () => {
 
     //Atividade & Status.
-    const atividade = [{ name: `discord.gg/animesbrasil`, type: 0 }];
+    const atividade = [{ name: `gg/animesbrasil`, type: 0 }];
     const status = [`online`];
 
     let random1 = 0;
