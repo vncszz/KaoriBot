3aaf26cf1b3ef9f676601f78bbdafc1a261feca8
mav <ademarvincius01@gmail.com>
2023-03-16 10:56:24 -0300

    Update

--- SlashCommands/mods/invite.js
+++ SlashCommands/mods/invite.js
@@ -11,11 +11,11 @@ module.exports = {
     if(!interaction.member.permissions.has(Discord.PermissionFlagsBits.ManageMessages)) {
         interaction.reply({content: `Você não tem permissão para utilizar esse comando.`, ephemeral: true})
     } else {
-        interaction.reply({content: `**☕ __Anime's Zero__
+        interaction.reply({content: `** ☕ __Anime's Zero__
         ・Seja muito bem vindo(a) a nosso servidor
-        Somos uma comunidade Geek que buscamos proporcionar Amizades & entretenimento para nossos membros.
+        ・Somos uma comunidade Geek que buscamos proporcionar Amizades & entretenimento para nossos membros.
         
-        Aqui temos:\n🎋・Servidor organizado a todo o público.\n🔨・Vagas staff abertas.\n🚀・Beneficios vip/booster.\n🤝・Parceria sempre abertas.\n💬・Server ativo.\n🖼️・Banner: https://imgur.com/sLi3dbS.png\n💐・Convite: https://discord.gg/animesbrasil`, ephemeral: true})
+        ・Aqui temos:\n🎋・Servidor organizado a todo o público.\n🔨・Vagas staff abertas.\n🚀・Beneficios vip/booster.\n🤝・Parceria sempre abertas.\n💬・Server ativo.\n🖼️・Banner: https://imgur.com/sLi3dbS.png\n💐・Convite: https://discord.gg/animesbrasil**`, ephemeral: true})
     }
   }
 }
