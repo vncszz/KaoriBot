23891d3a8e13114d9525c8b54f0079b0e6b6d3ba
mav <ademarvincius01@gmail.com>
2023-03-21 21:19:03 -0300

    Naomi Bot™

--- index.js
+++ index.js
@@ -17,12 +17,9 @@ console.clear()
 
 const { loadEvents } = require("./handler/handlerEvents");
 const { loadCommands } = require("./handler/handlerCommands");
-const { loadModals } = require('./Eventos/function/modalCreate');
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
-client.modals = new Discord.Collection();
-loadModals(client);
 
 
 //database connect
@@ -45,3 +42,23 @@ client.login(process.env.token).then(() => {
   loadEvents(client);
   loadCommands(client);
 });
+
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isModalSubmit()) return;
+
+  if (interaction.customId === 'partner') {
+
+    const invite = interaction.fields.getTextInputValue('invitePartner')
+    const idPartner = interaction.fields.getTextInputValue('idPartner')
+
+    let canal = interaction.guild.channels.cache.get('1076316523540533309') // id do canal
+    //let notificationId = '988493127331508224' //id do cargo de notificação
+
+    await interaction.reply({
+      content: `Parceria Enviada com sucesso. <:awp_c_1:1065717312071684096>`, ephemeral: false
+    })
+
+    canal.send({ content: `${invite}\nRep: <@${idPartner}>\nPromotor: \`${interaction.user.username}\`` });
+  }
+})
